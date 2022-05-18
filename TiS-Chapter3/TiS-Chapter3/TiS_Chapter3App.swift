47b92306c636b93ea0dcb09159c576d7221e49b5
Rahul Sharma <rahulrs0029@gmail.com>
2022-05-18 17:15:56 +0530

    Added Chapter 3 exercise.

---
+++ TiS-Chapter3/TiS-Chapter3/TiS_Chapter3App.swift
@@ -0,0 +1,17 @@
+//
+//  TiS_Chapter3App.swift
+//  TiS-Chapter3
+//
+//  Created by Rahul Sharma on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TiS_Chapter3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
